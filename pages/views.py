d181052fae768c572d1f3c3628aa230cefb834b4
MarvinBraun <34036123+MarvinBraun@users.noreply.github.com>
2019-08-02 16:28:58 +0200

    Passing objects from views.py to home.html

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from carstation.models import Carstation
+
 
 # Create your views here.
 
 def home_view(request,*args, **kwargs):
-	return render(request, "home.html",{})
+	objects = Carstation.objects.values()
+	carstations = []
+
+	for carstation in objects:
+		carstations.append([str(carstation['name']),float(carstation['lengtitude']),float(carstation['longtitude'])])
+		print(carstation['name'] + " " + str(carstation['lengtitude']))
+
+	print(type(carstations))
+
+	return render(request, "home.html",{'carstations':carstations})
 
 def contact_view(request,*args, **kwargs):
 	return render(request, "contact.html", {})
